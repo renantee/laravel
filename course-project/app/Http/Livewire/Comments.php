2b5bca79f5599c43ffe8af7798f832d1f62454ca
Renante D. Entera <renantee@codev.com>
2021-01-08 14:57:47 +0800

    Course Project: Livewire Tutorial (Updated - part 2)
    Update includes:
    > Properties and Actions
    > Create Migration and Model for Comments
    > Mount Event, Fetch from DB
    > Store Comment with Livewire
    > Real-Time Validation
    > Passing Data to Component
    > Create Ability to Remove Comment
    
    Execute commands:
    > php artisan make:migration create_comments_table
    > php artisan make:model Comment
    > php artisan migrate

--- course-project/app/Http/Livewire/Comments.php
+++ course-project/app/Http/Livewire/Comments.php
@@ -2,33 +2,44 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Comment;
 use Carbon\Carbon;
 use Livewire\Component;
 
 class Comments extends Component
 {
-    public $comments = [
-        [
-            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi ex cupiditate quo commodi aspernatur delectus veniam necessitatibus.',
-            'created_at' => '3 min ago',
-            'creator' => 'Renante'
-        ]
-    ];
+    public $comments;
 
     public $newComment;
 
+    public function mount()
+    {
+        $this->comments = Comment::latest()->get();
+    }
+
+    public function updated($field)
+    {
+        $this->validateOnly($field, ['newComment' => 'required|max:255']);
+    }
 
     public function addComment()
     {
-        if ($this->newComment == '') {
-            return;
-        }
-        array_unshift($this->comments, [
-            'body' => $this->newComment,
-            'created_at' => Carbon::now()->diffForHumans(),
-            'creator' => 'Entera'
+        $this->validate(['newComment' => 'required|max:255']);
+
+        $createdComment = Comment::create([
+            'body' => $this->newComment, 'user_id' => 1
         ]);
+
+        $this->comments->prepend($createdComment);
         $this->newComment = "";
+    }
+
+    public function remove($commentId)
+    {
+        $comment = Comment::find($commentId);
+        $comment->delete();
+
+        $this->comments = $this->comments->except($commentId);
     }
 
     public function render()
