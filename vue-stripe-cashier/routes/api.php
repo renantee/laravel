8b3052dd644ebb8c31d2c27f289f0ece68b022c9
Renante D. Entera <renantee@codev.com>
2021-01-29 04:00:06 +0800

    Shopping Cart: Vue Stripe Cashier
    Update includes:
    > Migrations and Data Structure
    > Database Seeding
    > Scaffolding the API
    > Building the Vue Frontend
    > Installing Stripe and Cashier
    
    Execute commands:
    > php artisan make:model Product --migration
    > php artisan make:model Category --migration
    > php artisan make:model Order --migration
    > php artisan make:migration create_order_product_table --create=order_product
    > php artisan make:migration create_category_product_table --create=category_product
    > composer require laravel/cashier
    > php artisan migrate
    > php artisan db:seed
    > npm install --save-dev @stripe/stripe-js
    > npm install && npm run dev

--- vue-stripe-cashier/routes/api.php
+++ vue-stripe-cashier/routes/api.php
@@ -2,18 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{category}', [CategoryController::class, 'show']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{product}', [ProductController::class, 'show']);
+
+Route::post('/purchase', [UserController::class, 'purchase']);
